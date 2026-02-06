93b4d61e83ff00caadd61098b49c5d1130366696
samzzry <samuelhumanez6@gmail.com>
2026-02-06 06:47:21 -0500

    Empleado_modelo c
    Updated the Empleado_modelo class to include 'telefono' and additional getter/setter methods.

--- proyecto api/empleado_modelo.py.py
+++ proyecto api/empleado_modelo.py.py
@@ -1,14 +1,28 @@
-class Empleado_modelo: 
-    def __init__(self, nombre, apellido, celular, cedula):
-        self.cedula_empleado = cedula
-        self.nombre_empleado = nombre
-        self.apellido_empleado = apellido
-        self.celular_empleado = celular
-        print("Empleado creado como objeto")
+class Empleado_modelo:
+    def __init__(self, nombre, apellido, cedula, telefono):
+        self.nombre_empleado=nombre
+        self.apellido_empleado=apellido
+        self.cedula_empleado=cedula
+        self.telefono_empleado=telefono
+        
+    def set_nombre_empleado(self,nombre):
+        self.nombre_empleado=nombre
+        
     def get_nombre_empleado(self):
         return self.nombre_empleado
-    def set_nombre_empleado(self, nuevo_nombre):
-        self.nombre_empleado = nuevo_nombre
-    def ver_info_empleado(self):
-        info_empleado = "nombre empleado" + self.nombre_empleado + " apellido empleado " + self.apellido_empleado  + " cedula empleado " + self.cedula_empleado + " celular empleado " + self.celular_empleado
-        return info_empleado
+    def set_apellido_empleado(self,nuevo_apellido):
+        self.apellido_empleado=nuevo_apellido
+    def get_apellido_empleado(self):
+        return self.apellido_empleadoj
+    def set_cedula_empleado(self,nueva_cedula):
+        self.cedula_empleado=nueva_cedula
+    def get_cedula_empleado(self):
+        return self.cedula_empleado
+    def set_telefono_empleado(self,nuevo_telefono):
+        self.telefono_empleado=nuevo_telefono
+    def get_telefono_empleado(self):
+        return self.telefono_empleado
+    def ver_info(self):
+        info="Nombre empleado " + self.nombre_empleado + "Apellido empleado " + self.apellido_empleado
+        info=info+ "cedula empleado: " + self.cedula_empleado +"telefono empleado"+self.telefono_empleado
+       
