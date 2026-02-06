28b82f9a2400fb8f5044dfdffa22fb2a4a803e27
samzzry <samuelhumanez6@gmail.com>
2026-02-06 06:37:59 -0500

    empleadomodelo

--- proyecto api/empleado_modelo.py.py
+++ proyecto api/empleado_modelo.py.py
@@ -5,13 +5,10 @@ class Empleado_modelo:
         self.apellido_empleado = apellido
         self.celular_empleado = celular
         print("Empleado creado como objeto")
-
     def get_nombre_empleado(self):
         return self.nombre_empleado
-    
     def set_nombre_empleado(self, nuevo_nombre):
         self.nombre_empleado = nuevo_nombre
-    
     def ver_info_empleado(self):
         info_empleado = "nombre empleado" + self.nombre_empleado + " apellido empleado " + self.apellido_empleado  + " cedula empleado " + self.cedula_empleado + " celular empleado " + self.celular_empleado
         return info_empleado
